b5d4062a31119921988ba4adfead28acefe0bce7
JueViGrace <juev1grace@gmail.com>
2025-10-25 14:54:06 +0000

    w

---
+++ modules/home-manager/hyprland.nix
@@ -0,0 +1,14 @@
+{pkgs, ...}: {
+  programs.hyprland = {
+    enable = true;
+  };
+  wayland.windowManager.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+    package = pkgs.hyprland;
+    systemd = {
+      enable = true;
+      variables = ["--all"];
+    };
+  };
+}
