a9b66d68c51dbc166e75c05d8f7cc28e08b12fb1
Daniel Zabala <juev1grace@gmail.com>
2025-11-07 18:33:25 -0400

    start ssh agent

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -113,6 +113,7 @@
       PasswordAuthentication = false;
     };
   };
+  programs.ssh.startAgent = true;
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "25.05";
